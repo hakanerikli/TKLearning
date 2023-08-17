7ea3ef70f64611de6e86e6c8cf81a296c2a8a85b
Hakan Erikli <hakanerikli@gmail.com>
2023-08-17 11:17:19 +0300

    grid code added.

--- main.py
+++ main.py
@@ -12,15 +12,18 @@ def click_button():
 my_label = tkinter.Label(text="Hello Tkinkter", font=("Arial",30, "normal"))
 # my_label.config(bg="black", fg="white")
 # my_label.pack(side="right")
-my_label.place(x=0, y=0)
+# my_label.place(x=0, y=0)
+my_label.grid(row=0, column=0)
 
 # Button
 my_button = tkinter.Button(text="Gönder", command=click_button, width=30)
 # my_button.pack(side="right")
-my_button.place(x=100, y=100)
+# my_button.place(x=100, y=100)
+my_button.grid(row=1, column=1)
 
 # Entry
 my_entry = tkinter.Entry(width=20)
 # my_entry.pack(side="right")
+my_entry.grid(row=2, column= 2)
 
 window.mainloop()
